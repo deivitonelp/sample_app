198a740fa489ed5a7ff0cd5fe0fd72ecd9512dec
Deivid Perez <deivitone@gmail.com>
2013-09-17 12:10:30 +0200

    Finished static pages Contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,5 +43,19 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact"  do
+ 	it "should have the content 'Contact'" do 
+ 		visit '/static_pages/contact'
+ 		page.should have_selector('h1', :text => 'Contact')
+ 	end	
+
+ 	it "should have the content 'Contact'" do 
+ 		visit '/static_pages/contact'
+ 		 page.should have_selector('title',
+ 					:texto => "Ruby on Rails Tutorial Sample App | Contact")
+ 	end
+
+  end
 end
 
